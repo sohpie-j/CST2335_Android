d3c2f10704b5915eba312f9c30ff41c2adb9c99c
sohpie-j <ka.sophie.jang@gmail.com>
2024-09-19 14:14:25 -0400

    added 4 buttons to centre

--- lib/main.dart
+++ lib/main.dart
@@ -106,66 +106,25 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done by the _incrementCounter method above.
-    // The Flutter framework has been optimized to make rerunning build methods fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
 
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+       title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical axis because Columns are vertical (the cross axis would be horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint" action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: Column( mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget> [
-            const Padding (
-              padding: EdgeInsets.all(16.0),
-              //padding: EdgeInsets.fromLTRB(0.0, 20.0, 39.0, 0.0), child: Text('You have pushed the button this many times:', style: TextStyle(fontSize:60.0)),
-            ),
+            ElevatedButton(child:Text("Button1"), onPressed:buttonClicked),
+            ElevatedButton(child:Text("Button2"), onPressed:buttonClicked),
+            ElevatedButton(child:Text("Button3"), onPressed:buttonClicked),
+            ElevatedButton(child:Text("Button4"), onPressed:buttonClicked),
+            ElevatedButton(child:Text("Button5"), onPressed:buttonClicked),
+            ElevatedButton(child:Text("Button6"), onPressed:buttonClicked),
 
-            TextField(controller: _loginController, decoration: const InputDecoration( hintText:"Type here", labelText: "Login", border: OutlineInputBorder()), ), //TextField
-            TextField(controller: _passwordController, obscureText:true, decoration: const InputDecoration( hintText:"Type here", labelText: "Password", border: OutlineInputBorder()), ), //TextField
-
-            // Padding(padding: EdgeInsets.all(16.0), child:Image.asset("images/algonquin.jpg", height: 600, width:800,) ),
-            //ElevatedButton( onPressed: buttonClicked,
-            ElevatedButton( onPressed: ()
-                          {
-                            setState(()
-                            {
-                                if (_passwordController.text == 'QWERTY123')
-                                {
-                                   imageSource = 'images/light.png';
-                                } else
-                                {
-                                   imageSource = 'images/stop.png';
-                                }
-                            });
-                          },
-              child: const Text('Login'),
-            ),
-
-            //const SizedBox(height: 20), Image.asset( imageSource, width: 300, height: 300, ),
-            Flexible( child: Image.asset( imageSource, width: 300, height: 300,),),
           ],
         ),
       ),
-      //floatingActionButton: FloatingActionButton( onPressed: _incrementCounter, tooltip: 'Increment', child: const Icon(Icons.add),), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 
